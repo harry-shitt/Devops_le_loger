9fc9a2f39ca4e4beb20f03c478bdf7bf161bec39
harry-shitt <harshithsalian335@gmail.com>
2022-04-27 01:04:16 +0530

    worked on finding distance and time using distance matrix

--- js/map.js
+++ js/map.js
@@ -1,5 +1,7 @@
 
-let map;
+
+let map , address , loc , loc_lat ,loc_lng;
+let lat_lng_list= []
 let markers = [];
 let amenditiesmarekr = [];
 let rentalmarker = [];
@@ -9,7 +11,7 @@ let response;
 var place , location;
 let rentallocationlist =[];
 
-function getString(price  , buildingname , roomtype , desc)
+function getString(price  , buildingname , roomtype , desc , dist , time)
 {
     
     let contentString =
@@ -17,10 +19,12 @@ function getString(price  , buildingname , roomtype , desc)
     '<div id="siteNotice">' +
     "</div>" +
     '<div id="bodyContent">' +
-    "<h3><b>Building Name</b>"+"&#160;"+ getprice(buildingname)+ "&#160;" +"</h3>"+
-    "<h3><b>Price</b>"+"&#160;"+ getbuildingname(price)+ "&#160;" +"</h3>"+
-    "<h3><b>Room</b>"+"&#160;"+ gettype(roomtype)+ "&#160;" +"</h3>"+
-    "<h3><b>Description</b>"+"&#160;"+ getdesc(desc)+ "&#160;" +"</h3>"+
+    "<h3><b>Building Name :</b>"+"&#160;"+ getbuildingname(buildingname)+ "&#160;" +"</h3>"+
+    "<h3><b>Price :</b>"+"&#160;"+ getprice(price)+ "&#160;" +"</h3>"+
+    "<h3><b>Room :</b>"+"&#160;"+ gettype(roomtype)+ "&#160;" +"</h3>"+
+    "<h3><b>Description : </b>"+"&#160;"+ getdesc(desc)+ "&#160;" +"</h3>"+
+    "<h3><b>Distance from location :</b>"+"&#160;"+ dist+ "&#160;" +"</h3>"+
+    "<h3><b>Average time to reach the location : </b>"+"&#160;"+ time + "&#160;" +"</h3>"+
     "</div>" +
     "</div>";
     return contentString;
@@ -52,12 +56,13 @@ function setMapOnAll(map) {
   for (let i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
   }
+
 }
 
-function hideMarkers() {
-  setMapOnAll(null);
+function hideMarkers()
+{
+    setMapOnAll(null);
 }
-
 
 function deleteMarkers() {
   hideMarkers();
@@ -102,6 +107,22 @@ function deleterentalMarkers() {
     rentalmarker = [];
 }
 
+function percentageinserted(s)
+{
+    let temp = '';
+    for(var i=0;i<s.length;i++)
+    {
+        if(s[i] == " ")
+        {
+            temp += '%20';
+        }
+        else
+        {
+            temp += s[i];
+        }
+    }
+    return s;
+}
 
 
 function initMap()
@@ -117,30 +138,33 @@ function initMap()
     });
     markers.push(marker);
     setMapOnAll(map);
-    geocoder = new google.maps.Geocoder();    
+    geocoder = new google.maps.Geocoder();  
 }
 
 
   function codeAddress(address) 
     {
-        deleteMarkers()
-        deleteamenditiesMarkers()
-        deleterentalMarkers()
+        lat_lng_list = []
+        deleteMarkers();
+        deleteamenditiesMarkers();
+        deleterentalMarkers();
         geocoder.geocode( {address:address}, function(results, status) 
         {
             if (status == google.maps.GeocoderStatus.OK) 
             {
-            map.setCenter(results[0].geometry.location);//center the map over the result
-            var marker = new google.maps.Marker(
-            {
-                map: map,
-                position: results[0].geometry.location
-            });
-            markers.push(marker);
-            setMapOnAll(map);
+                loc_lat = results[0].geometry.location.lng();
+                console.log(loc_lat)
+                map.setCenter(results[0].geometry.location);//center the map over the result
+                var marker = new google.maps.Marker(
+                {
+                    map: map,
+                    position: results[0].geometry.location
+                });
+                markers.push(marker);
+                setMapOnAll(map);
             } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
         });
     }
 
@@ -149,14 +173,10 @@ function initMap()
 
 function finddistance()
 {
-
-    
-
-
-
-
+  
     for(var  i=0 ; i < rentallocationlist.length;i++)
     {
+        console.log(rentallocationlist[i][14])
         let marker = new google.maps.Marker(
         {
             map: map,
@@ -165,7 +185,7 @@ function finddistance()
             opacity : 10,
         });
         let infowindow = new google.maps.InfoWindow({
-            content: getString(rentallocationlist[i][0] , rentallocationlist[i][6] , rentallocationlist[i][7] , rentallocationlist[i][8]),
+            content: getString(rentallocationlist[i][0] , rentallocationlist[i][6] , rentallocationlist[i][7] , rentallocationlist[i][8] ,rentallocationlist[i][13] , rentallocationlist[i][14] ),
         });
         marker.addListener("click", () => {
             infowindow.open({
@@ -182,49 +202,34 @@ function finddistance()
 
 function findplace()
 {
-   
+
     place = document.getElementById('placename');
-    location = document.getElementById('location');
+    loc = document.getElementById('location');
     place = place.value;
-    location = location.value;
+    loc = loc.value;
     document.getElementById('placename').value = "";
     document.getElementById('location').value = "";
 
 
-    address = place +" ," + location;
+    address = place +" ," + loc;
     codeAddress(address);
 
    var clustringnodelist = [] , clusteringcenterlist = [];
    let url1=new URL( "http://localhost:8000/locationresult")
-   url1.searchParams.append('location',location)
+   url1.searchParams.append('location',loc)
     fetch(url1)
     .then(response => response.json())
     .then(data => {
         data = JSON.parse(data);
         rentallocationlist = [];
+        amenditiesmarekr = [];
+        deleteamenditiesMarkers();
+        deleterentalMarkers();
         for(var i=0;i<data.length;i++)
         {
             if(data[i].length > 7)
             {
-                rentallocationlist.push(data[i]);
-                // let marker = new google.maps.Marker(
-                // {
-                //     map: map,
-                //     position: {lat:data[i][1] , lng:data[i][2]},
-                //     icon : "assets/images/house.png",                    
-                //     opacity : 10,
-                // });
-                // let infowindow = new google.maps.InfoWindow({
-                //     content: getString(data[i][0] , data[i][6] , data[i][7] , data[i][8]),
-                // });
-                // marker.addListener("click", () => {
-                //     infowindow.open({
-                //         anchor: marker,
-                //         map,
-                //         shouldFocus: true,
-                //     });
-                // });
-                // rentalmarker.push(marker);   
+                rentallocationlist.push(data[i]);  
             }
             else
             {
@@ -240,7 +245,7 @@ function findplace()
             }
         }
         finddistance();
-        setamenditiesmarker(map);
+        setamenditiesmarker(map)
     });   
 
 
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from urllib.request import Request
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from numpy import source
 import requests
 import json
 import uvicorn
@@ -8,7 +9,11 @@ import pandas as pd
 import array as arr  
 import sys
 import math
+import requests
 
+
+source_lat_lng = []
+rental_lat_lng = []
 
 app = FastAPI()
 origins=["*"]
@@ -25,17 +30,28 @@ def root():
 def root(location:str):
     locationresult = k_mediod(location)
     return  locationresult
+
+# @app.get("/distance")
+# def root(lat_lng_list):
+#     print(lat_lng_list)
+#     return 1
+
+
     
-@app.get("/locationresult2")
+@app.get("/havershine")
 def root(location:str):
-    locationresult = k_mediod(location , 2)
+    locationresult = havershine(location)
     return  locationresult
 
 def k_mediod(location):
+    
+    
+
 
     location = location.lower()
     location = location.replace(" " , "")
     city = location
+    
 
     rental_data = pd.read_csv(r"Data/Rental_Data.csv",encoding='utf-8')
     atm_data = pd.read_csv(r"Data/Atm_Data.csv",encoding='utf-8')
@@ -49,13 +65,54 @@ def k_mediod(location):
     restaurent_data = restaurent_data[restaurent_data["City"] == city]
 
 
+
+
+    ###############3 finding distance###############
+
+    #Geoding location
+    prime_lat_lng = []
+    x = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyCl4IFUvzMQSDXl8jJdIn9BtX9b5Z8Gj2A')
+    jsonresposne = json.loads(x.text)
+    temp_lat_lng = jsonresposne['results'][0]['geometry']['location']
+    source_lat_lng.append(temp_lat_lng)
+
+
+    ## Extracting rental location latitude and longitude
+    rental_lat_lng = rental_data[['Latitude' , 'Longitude']].values.tolist()
+    slat = source_lat_lng[0]['lat']
+    slng = source_lat_lng[0]['lng']
+    url_string = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+str(slat)+"%2C"+str(slng)+"&destinations="
+    for i in range(len(rental_lat_lng)):
+        if i == len(rental_lat_lng)-1:
+            url_string = url_string + str(rental_lat_lng[i][0])
+            url_string = url_string + "%2C"
+            url_string = url_string + str(rental_lat_lng[i][1])
+        else :
+            url_string = url_string + str(rental_lat_lng[i][0])
+            url_string = url_string + "%2C"
+            url_string = url_string + str(rental_lat_lng[i][1])
+            url_string = url_string + "%7C"     
+
+    url_string = url_string + "&key=AIzaSyCl4IFUvzMQSDXl8jJdIn9BtX9b5Z8Gj2A"
+    payload={}
+    headers = {}
+    response = requests.request("GET", url_string, headers=headers, data=payload)
+    rentalresposne = json.loads(response.text)
+    rental_lat_lng = []
+    for i in range(len(rentalresposne['rows'][0]['elements'])):
+        dist = rentalresposne['rows'][0]['elements'][i]['distance']['text']
+        duration = rentalresposne['rows'][0]['elements'][i]['duration']['text']
+        rental_lat_lng.append([dist , duration])
+    
+
+    #############################
+
     # # concatinaitng data
     atm_data = atm_data[["Address" , "Latitude" , "Longitude","Easting" , "Northing" , "Type"]]
     hospital_data = hospital_data[["Address" , "Latitude" , "Longitude","Easting" , "Northing" , "Type"]]
     restaurent_data = restaurent_data[["Address" , "Latitude" , "Longitude","Easting" , "Northing" , "Type"]]
     data = pd.concat([atm_data, hospital_data , restaurent_data], axis=0, ignore_index=True)
 
-    
 
     #colouring rental
     uniquecolor = len(rental_data)
@@ -64,11 +121,8 @@ def k_mediod(location):
         colorlist.append(i)
     rental_data['color'] = colorlist
 
-
-    # if(type == 2):
-    #     rental_data_list = rental_data.values.tolist()
-    #     rentaljson = json.dumps(rental_data_list)
-    #     return rentaljson
+    
+   
 
     #filering latitude and longitude
     rental_list = rental_data[["Easting" , "Northing" , "color"]].values.tolist()
@@ -97,16 +151,173 @@ def k_mediod(location):
 
     datalist = data.values.tolist()
     rental_data_list = rental_data.values.tolist()
+
+    for i in range(len(rental_data_list)):
+        rental_data_list[i].append(rental_lat_lng[i][0])
+        rental_data_list[i].append(rental_lat_lng[i][1])
+        print(rental_lat_lng[i][1])
+
     datalist = rental_data_list + datalist
     datajson = json.dumps(datalist)
 
-    # data = data.to_json(orient='column')
+    
 
     return datajson    
 
 
 
+
+
+def havershine(location):
+    location = location.lower()
+    location = location.replace(" " , "")
+    city = location
+
+    rental_data = pd.read_csv(r"Data/Rental_Data.csv",encoding='utf-8')
+    atm_data = pd.read_csv(r"Data/Atm_Data.csv",encoding='utf-8')
+    hospital_data = pd.read_csv(r"Data/Hospital_Data.csv",encoding='utf-8')
+    restaurent_data = pd.read_csv(r"Data/Restaurent_Data.csv",encoding='utf-8')
+
+
+    rental_data = rental_data[rental_data["City"] == city]
+    atm_data = atm_data[atm_data["City"] == city]
+    hospital_data = hospital_data[hospital_data["City"] == city]
+    restaurent_data = restaurent_data[restaurent_data["City"] == city]
+
+
+    atm_data = atm_data[["Address" , "Latitude" , "Longitude" "Type"]]
+    hospital_data = hospital_data[["Address" , "Latitude" , "Longitude", "Type"]]
+    restaurent_data = restaurent_data[["Address" , "Latitude" , "Longitude", "Type"]]
+
+    locationlist = rental_data[["Latitude" , "Longitude"]].values.tolist()
+    atmlist = atm_data[["Latitude" , "Longitude"]].values.tolist()
+    restaurentlist = restaurent_data[["Latitude" , "Longitude"]].values.tolist()
+    hospitallist = hospital_data[["Latitude" , "Longitude"]].values.tolist()
+    
+
+    restaurent_sum_list = []
+    i = 0
+    for location in locationlist:
+        currlat = float(location[1])
+        currlong = location[2]
+        sum = 0
+        counter = 0
+        for restaurent in restaurentlist:
+            curr_rest_lat = float(restaurent[1])
+            curr_rest_long = float(restaurent[2])
+            dLat = (currlat - curr_rest_lat) * math.pi / 180.0
+            dLon = (currlong - curr_rest_long) * math.pi / 180.0
+            # convert to radians
+            lat1 = (currlat) * math.pi / 180.0
+            lat2 = (curr_rest_lat) * math.pi / 180.0
+            # apply formulae
+            a = (pow(math.sin(dLat / 2), 2) +
+                pow(math.sin(dLon / 2), 2) *
+                    math.cos(lat1) * math.cos(lat2))
+            rad = 6371
+            c = 2 * math.asin(math.sqrt(a))
+            if rad*c <= 2:
+                sum = sum + rad*c
+                counter += 1
+        # print(rad*c , currlat , curr_rest_lat , currlong , curr_rest_long)
+    if counter==0:
+        restaurent_sum_list.append([currlat , currlong , sys.maxint , i])
+    else:
+        restaurent_sum_list.append([currlat , currlong , sum/counter , i])
+    i += 1
+
+    restaurent_sum_list = sorted(restaurent_sum_list, key = lambda x: float(x[2]))
+
+
+    atm_sum_list = []
+    i = 0
+    for location in locationlist:
+        currlat = float(location[1])
+        currlong = location[2]
+        sum = 0
+        counter = 0
+        for atm in atmlist:
+            curr_rest_lat = atm[1]
+            curr_rest_long = atm[2]
+            dLat = (currlat - curr_rest_lat) * math.pi / 180.0
+            dLon = (currlong - curr_rest_long) * math.pi / 180.0
+            # convert to radians
+            lat1 = (currlat) * math.pi / 180.0
+            lat2 = (curr_rest_lat) * math.pi / 180.0
+            # apply formulae
+            a = (pow(math.sin(dLat / 2), 2) +
+                pow(math.sin(dLon / 2), 2) * math.cos(lat1) * math.cos(lat2))
+            rad = 6371
+            c = 2 * math.asin(math.sqrt(a))
+            if rad*c <= 2:
+                sum += rad*c
+                counter += 1
+    if counter==0:
+        atm_sum_list.append([currlat , currlong , 10000 , i])
+    else:
+        atm_sum_list.append([currlat , currlong , sum/counter , i])
+    i += 1
+
+    atm_sum_list = sorted(atm_sum_list, key = lambda x: float(x[2]))
+
+    hospital_sum_list = []
+    i = 0
+    for location in locationlist:
+        currlat = float(location[1])
+        currlong = location[2]
+        sum = 0
+        counter = 0
+        for hospital in hospitallist:
+            curr_rest_lat = hospital[1]
+            curr_rest_long = hospital[2]
+            dLat = (currlat - curr_rest_lat) * math.pi / 180.0
+            dLon = (currlong - curr_rest_long) * math.pi / 180.0
+            # convert to radians
+            lat1 = (currlat) * math.pi / 180.0
+            lat2 = (curr_rest_lat) * math.pi / 180.0
+            # apply formulae
+            a = (pow(math.sin(dLat / 2), 2) +
+                pow(math.sin(dLon / 2), 2) *
+                    math.cos(lat1) * math.cos(lat2))
+            rad = 6371
+            c = 2 * math.asin(math.sqrt(a))
+            if rad*c <= 2:
+                sum += rad*c
+                counter += 1
+    if counter == 0:
+        hospital_sum_list.append([currlat , currlong , 1000 , i])
+    else:
+        hospital_sum_list.append([currlat , currlong , sum/counter , i])
+    i += 1
+
+    hospital_sum_list = sorted(hospital_sum_list, key = lambda x: float(x[2]))
+    
+    mydict = {}
+    for k in range(len(restaurent_sum_list)):
+        if restaurent_sum_list[k][3] in mydict :
+            val = mydict[restaurent_sum_list[k][3]]
+            mydict.update({restaurent_sum_list[k][3] : val+k})
+        else:
+            mydict.update({restaurent_sum_list[k][3] : k})
+        
+        if atm_sum_list[k][3] in mydict :
+            val = mydict[atm_sum_list[k][3]]
+            mydict.update({atm_sum_list[k][3] : val+k})
+        else:
+            mydict.update({atm_sum_list[k][3] : k})
+        
+        if hospital_sum_list[k][3] in mydict :
+            val = mydict[hospital_sum_list[k][3]]
+            mydict.update({hospital_sum_list[k][3] : val+k})
+        else:
+            mydict.update({hospital_sum_list[k][3] : k})
+    sorted_dict = sorted(mydict.items(), key = lambda kv:(kv[1], kv[0]))
+    for ele in sorted_dict:
+        print(locationlist[ele[0]][2])
+
+    print(locationlist)
 uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
 
 
